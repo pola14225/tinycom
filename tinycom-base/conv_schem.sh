6ec18872c2f89a94e06a5ec05d940d57b86395a0
David Janssens <dj@netforce.com>
2016-02-04 14:00:51 +0700

    update base

--- tinycom-base/conv_schem.sh
+++ tinycom-base/conv_schem.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp output.pdf tinycom-base-layout.pdf
+mv output.pdf tinycom-base-layout.pdf
 convert -density 300 tinycom-base-layout.pdf -quality 90 /tmp/layout.png
 convert -rotate 90 /tmp/layout.png tinycom-base-layout.png
