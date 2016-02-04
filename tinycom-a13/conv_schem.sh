6b95bbf9f54b76521937f9fd30c5490f4d22a2f0
David Janssens <dj@netforce.com>
2016-02-04 13:26:35 +0700

    a13 updte

--- tinycom-a13/conv_schem.sh
+++ tinycom-a13/conv_schem.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp output.pdf tinycom-a13-layout.pdf
+mv output.pdf tinycom-a13-layout.pdf
 convert -density 300 tinycom-a13-layout.pdf -quality 90 /tmp/layout.png
 convert -rotate 90 /tmp/layout.png tinycom-a13-layout.png
