60918c93f1e2ae97fe6abb6e04567cdd692c6cea
David Janssens <dj@netforce.com>
2016-03-29 19:30:04 +0700

    rename

--- tinycom-base/conv_schem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mv output.pdf tinycom-base-layout.pdf
-convert -density 300 tinycom-base-layout.pdf -quality 90 /tmp/layout.png
-convert -rotate 90 /tmp/layout.png tinycom-base-layout.png
--- tinycom-video/conv_schem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp output.pdf tinycom-video-layout.pdf
-convert -density 300 tinycom-video-layout.pdf -quality 90 /tmp/layout.png
-convert -rotate 90 /tmp/layout.png tinycom-video-layout.png
